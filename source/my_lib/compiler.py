164162c8aa1f0cdcb18937da32f5b529249e2d71
Tiago Lourinho <tiagolourinho001@gmail.com>
2025-02-24 19:19:36 +0000

    added compiler class to gather executable and ptxs

---
+++ source/my_lib/compiler.py
@@ -0,0 +1,104 @@
+import os
+import subprocess
+from pathlib import Path
+
+from tqdm import tqdm
+
+
+class Compiler:
+    """Handles the compilation and PTX extraction of all the benchmarks"""
+
+    NVCC_FLAGS = [
+        f"-gencode=arch=compute_52,code=compute_52",  # Default architecture used by nvcc but explicit
+        "-O0",  # No optimization
+        "-Xcompiler",
+        "-O0",  # No optimization for host compiler
+        "-Xptxas",
+        "-O0",  # No optimization for PTX assembler
+        "-lcuda",
+    ]
+    """Flags used during compilation"""
+
+    def __init__(self, benchmarks_folder: str):
+        self.__benchmarks_folder = benchmarks_folder
+
+    def compile_and_obtain_ptx(self):
+        """Obtains the PTX and executable of all the benchmarks composed of only 1 file"""
+
+        bin_folder = Path(self.__benchmarks_folder) / ".." / "bin"
+        bin_folder.mkdir(parents=True, exist_ok=True)
+
+        successfully_compiled = 0
+        compilation_errors = 0
+
+        # Gather all .cu files inside the benchmarks folder
+        cu_files = [
+            Path(root) / file
+            for root, _, files in os.walk(self.__benchmarks_folder)
+            for file in files
+            if file.endswith(".cu")
+        ]
+        assert len(cu_files) > 0, "No benchmark found."
+
+        # Go over all CUDA files and try to compile them
+        for cu_file_path in tqdm(cu_files, desc="Compiling CUDA Files", unit="file"):
+
+            relative_path = cu_file_path.relative_to(self.__benchmarks_folder)
+            safe_name = str(relative_path).replace("/", "_").replace(" ", "_")
+
+            ptx_file = bin_folder / f"{safe_name.replace('.cu', '.ptx')}"
+            executable_file = bin_folder / f"{safe_name.replace('.cu', '.out')}"
+
+            # NVCC commands
+            ptx_cmd = [
+                "nvcc",
+                *self.NVCC_FLAGS,
+                "-ptx",
+                str(cu_file_path),
+                "-o",
+                str(ptx_file),
+            ]
+            exe_cmd = [
+                "nvcc",
+                *self.NVCC_FLAGS,
+                str(cu_file_path),
+                "-o",
+                str(executable_file),
+            ]
+
+            # Try obtaining the PTX and then the executable
+            try:
+
+                subprocess.run(
+                    ptx_cmd,
+                    check=True,
+                    stdout=subprocess.PIPE,
+                    stderr=subprocess.PIPE,
+                )
+
+                subprocess.run(
+                    exe_cmd,
+                    check=True,
+                    stdout=subprocess.PIPE,
+                    stderr=subprocess.PIPE,
+                )
+
+                successfully_compiled += 1
+
+            except subprocess.CalledProcessError:
+                compilation_errors += 1
+
+                # Remove PTX if executable compilation fails
+                if ptx_file.exists():
+                    ptx_file.unlink()
+
+        total = successfully_compiled + compilation_errors
+        success_rate = (successfully_compiled / total) * 100
+        print(
+            f"\n{successfully_compiled}/{total} CUDA files successfully compiled ({success_rate:.2f}%)."
+        )
+
+
+if __name__ == "__main__":
+    compiler = Compiler(benchmarks_folder="benchmarks")
+    compiler.compile_and_obtain_ptx()
